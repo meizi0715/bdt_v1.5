674c32df67ff52d29684482fc00e773733eb1a46
meizi0715 <gongdaomei@gmail.com>
2026-03-18 10:25:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -426,7 +426,7 @@ async def main(f=None):
         # 差分比较
         files = sorted(f for f in os.listdir(OUTPUT_DIR) if f.endswith(".txt"))
         if len(files) >= 2:
-            file_old = os.path.join(OUTPUT_DIR, files[-2])
+            file_old = os.path.join(OUTPUT_DIR, files[-3])
             if compare_files(file_old, file_new):
 
                 #===========v1.6 2026/03/10 Add Start
